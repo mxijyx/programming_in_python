49cd99124ad4d815b8649b4cb622e74a9b273864
Maja <67898314+mxijyx@users.noreply.github.com>
2025-12-07 20:18:02 +0100

    asgmt2 ready

--- mm_asgmt2.py
+++ mm_asgmt2.py
@@ -31,7 +31,7 @@ class Sheep:
     def __init__(self, index, position: tuple):
         self.index = index
         self._position = position
-        self.alive = False
+        self.alive = True
         self.step = SHEEP_STEP
 
         logger.debug(f'Sheep number {self.index} initialized on position {position}')
@@ -71,6 +71,8 @@ class Wolf:
     def move(self, herd: list['Sheep']):
         logger.info('Wolf is chasing!')
 
+        herd = [s for s in herd if s.alive]
+
         self.prey = herd[0]
         for sheep in herd[1:]:
             if euclidean_distance2(self.position, sheep.position) < euclidean_distance2(self.position, self.prey.position):
@@ -106,6 +108,8 @@ if __name__ == '__main__':
     max_rounds = MAX_ROUNDS
     sheep_number = SHEEP_NUMBER
     position_limit = POSITION_LIMIT
+    sheep_step = SHEEP_STEP
+    wolf_step = WOLF_STEP
 
     arg_parser = ArgumentParser(
         prog = 'Chasing Sheep',
@@ -113,7 +117,7 @@ if __name__ == '__main__':
     )
 
     arg_parser.add_argument('-c', '--config', type=str, help='Load configuration from .ini file')
-    arg_parser.add_argument('-l', '-log', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Logging level')
+    arg_parser.add_argument('-l', '--log', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='Logging level')
     arg_parser.add_argument('-r', '--rounds', type=int, default=MAX_ROUNDS, help='Number of sheep rounds')
     arg_parser.add_argument('-s', '--sheep', type=int, default=SHEEP_NUMBER, help='Sheep number')
     arg_parser.add_argument('-w', '--wait', action='store_true', help='Wait until sheep moved for a set time')
@@ -126,7 +130,7 @@ if __name__ == '__main__':
         logging.disable()
 
     if args.rounds:
-        if args.round <= 0:
+        if args.rounds <= 0:
             raise ValueError('Round cannot be less than 0')
         max_rounds = args.rounds
 
@@ -142,16 +146,16 @@ if __name__ == '__main__':
         if os.path.isfile(args.config):
             config.read(args.config)
 
-            position_limit = float(['Sheep'], ['InitPosLimit'])
+            position_limit = config.getfloat('Sheep', "InitPosLimit")
             if position_limit <= 0: raise ValueError('Position limit must be positive')
 
-            sheep_step = float(['Sheep'], ['MoveDist'])
+            sheep_step = config.getfloat('Sheep', 'MoveDist')
             if sheep_step <= 0: raise ValueError('Sheep step must be positive')
 
-            wolf_step = float(['Wolf'], ['MoveDist'])
+            wolf_step = config.getfloat('Wolf', 'MoveDist')
             if wolf_step <= 0: raise ValueError('Wolf step must be positive')
 
-            logger.debug(f'.ini configuration file loded with positition {position_limit}, sheep step {sheep_step}, wolf step {wolf_step}')
+            logger.debug(f'.ini configuration file loaded with position {position_limit}, sheep step {sheep_step}, wolf step {wolf_step}')
         else:
             raise FileNotFoundError('Configuration file not found')
 
@@ -163,37 +167,72 @@ if __name__ == '__main__':
     random.uniform(a, b)
     Return a random floating-point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
     '''
-    sheep_cor = uniform(-position_limit, position_limit)
-    herd = [Sheep(i, (sheep_cor, sheep_cor)) for i in range(sheep_number)]
-    alive_herd = [s for s in herd if s.alive ]
-    logger.info('Herd is ready for the game!')
+    herd = []
+    for i in range(sheep_number):
+        x = uniform(-position_limit, position_limit)
+        y = uniform(-position_limit, position_limit)
+        sheep = Sheep(i, (x, y))
+        herd.append(sheep)
+
+    logger.info('\nHerd is ready for the game!')
 
     wolf = Wolf((0,0))
 
     results = []
+    alive_after_r = []
 
     for r in range(max_rounds):
+        alive_herd = [s for s in herd if s.alive]
         logger.info(f'Round {r} started!')
-        print('New round started! Get ready!')
+        print('--------------\nNew round started! Get ready!')
         print('Round: ', r, ' Alive sheep: ', len(alive_herd))
 
         for sheep in alive_herd:
             sheep.move()
         logger.info(f'All alive sheep have moved!')
 
+        print(f'Initial wolf position: ({wolf.position[0]:.3f}, {wolf.position[1]:.3f})')
+
         wolf.move(alive_herd)
 
+        print(f'Wolf moved to position: {wolf.position[0]:.3f}, {wolf.position[1]:.3f}')
+        if wolf.prey and not wolf.prey.alive:
+            print(f'Sheep {wolf.prey.index} was eaten!')
+        elif wolf.prey and wolf.prey.alive:
+            print(f'Sheep {wolf.prey.index} has escaped the wolf!')
+        alive_herd = [s for s in herd if s.alive]
 
+        results.append(
+            {
+                'round no': r,
+                'wolf_pos': wolf.position,
+                'sheep_pos': [s.position for s in herd]
+            }
+        )
+        alive_after_r.append(len(alive_herd))
+        logger.info(f'At the end of the round {r} we have {len(alive_herd)} alive sheep')
 
+        if not alive_herd:
+            logger.info('No more alive sheep! Simulation terminated!')
+            break
+        if args.wait:
+            input('Press Enter to continue...')
 
+    else:
+        logger.info(f'Simulation terminated after max rounds - {max_rounds} rounds!')
 
+    print('\n---GAME OVER---')
 
+    ########################################
 
+    json_file = 'pos.json'
 
+    with open(json_file, 'w') as f:
+        json.dump(results, f, indent=4)
+    logger.debug(f'Information was saved to location {os.path.abspath(json_file)}')
 
-
-
-
-
-
-
+    csv_file = 'alive.csv'
+    with open(csv_file, 'w') as f:
+        writer = csv.writer(f, delimiter='\t', lineterminator='\n')
+        writer.writerow(['Round Number', 'Number of alive sheep'])
+        writer.writerows(enumerate(alive_after_r))
